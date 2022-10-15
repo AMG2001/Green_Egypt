dca9726529c5e6f6ed6fdc59851de049595f2f89
Mohamad amgad elsaid <65943047+MavicaSoftwareDev@users.noreply.github.com>
2022-10-15 23:28:43 +0200

    added

--- green_egypt/lib/config/pages_names.dart
+++ green_egypt/lib/config/pages_names.dart
@@ -2,6 +2,7 @@ import 'package:get/get.dart';
 import 'package:green_egypt/screens/home_page/home_page.dart';
 import 'package:green_egypt/screens/introduction_screen/introduction_screen.dart';
 import 'package:green_egypt/screens/login_screen/login_screen.dart';
+import 'package:green_egypt/screens/register_page/register_page.dart';
 import 'package:green_egypt/screens/splach_screen/splach_screen.dart';
 import 'package:green_egypt/screens/start_screen/start_screen.dart';
 
@@ -11,6 +12,7 @@ class PagesNames {
   static String loginScreen = "/login";
   static String homePage = "/home";
   static String startScreen = "/start";
+    static String registerPage = "/register";
 
   static List<GetPage> listOfPages = [
     GetPage(
@@ -32,6 +34,10 @@ class PagesNames {
     GetPage(
         name: startScreen,
         page: () => StartScreen(),
+        transition: Transition.fade),
+            GetPage(
+        name: registerPage,
+        page: () => RegisterPage(),
         transition: Transition.fade)
   ];
 }
--- green_egypt/lib/screens/start_screen/start_screen.dart
+++ green_egypt/lib/screens/start_screen/start_screen.dart
@@ -1,5 +1,7 @@
 import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:green_egypt/config/pages_names.dart';
 
 class StartScreen extends StatelessWidget {
   const StartScreen({super.key});
@@ -28,6 +30,7 @@ class StartScreen extends StatelessWidget {
                     width: 12,
                   ),
                   AnimatedTextKit(
+                    repeatForever: true,
                     animatedTexts: [
                       TypewriterAnimatedText(
                         'Green_Egypt 🍀',
@@ -38,11 +41,21 @@ class StartScreen extends StatelessWidget {
                   ),
                 ],
               ),
+              SizedBox(
+                height: 24,
+              ),
               Text("Don't have an Account !"),
               SizedBox(
                 height: 12,
               ),
-              ElevatedButton(onPressed: () {}, child: Text("Register")),
+              ElevatedButton(
+                  style: ButtonStyle(
+                      backgroundColor:
+                          MaterialStateProperty.all<Color>(Colors.green)),
+                  onPressed: () {
+                    Get.toNamed(PagesNames.registerPage);
+                  },
+                  child: Text("Register")),
               SizedBox(
                 height: 12,
               ),
@@ -50,7 +63,14 @@ class StartScreen extends StatelessWidget {
               SizedBox(
                 height: 12,
               ),
-              ElevatedButton(onPressed: () {}, child: Text("Login")),
+              ElevatedButton(
+                  style: ButtonStyle(
+                      backgroundColor:
+                          MaterialStateProperty.all<Color>(Colors.green)),
+                  onPressed: () {
+                    Get.toNamed(PagesNames.loginScreen);
+                  },
+                  child: Text("Login")),
             ],
           ),
         ),
