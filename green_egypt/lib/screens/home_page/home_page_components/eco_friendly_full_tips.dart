c6cc1e0a8a319f4717df9f6c3b359f6d8c78eeb3
Mohamad amgad elsaid <65943047+MavicaSoftwareDev@users.noreply.github.com>
2023-02-05 22:13:05 +0200

    main page done

--- green_egypt/lib/screens/home_page/home_page_components/eco_friendly_full_tips.dart
+++ green_egypt/lib/screens/home_page/home_page_components/eco_friendly_full_tips.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
-
+import 'package:get/get.dart';
+import 'package:green_egypt/config/dimensions.dart';
+import 'package:responsive_sizer/responsive_sizer.dart';
+import 'package:green_egypt/screens/home_page/home_page_components/friendly_tips_item.dart';
 
 class EcoFriendlyFullTips extends StatelessWidget {
   const EcoFriendlyFullTips({super.key});
@@ -9,6 +12,204 @@ class EcoFriendlyFullTips extends StatelessWidget {
     /**
      * Implement List view with 15 tip .
      */
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Color(0xfffefefc),
+      body: Column(
+        children: [
+          Row(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [],
+          ),
+          Container(
+            width: Dimensions.width,
+            height: Dimensions.height,
+            decoration: const BoxDecoration(
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(32),
+                topRight: Radius.circular(32),
+              ),
+              color: Color(0xfff7f5f5),
+            ),
+            child: ListView(
+              scrollDirection: Axis.vertical,
+              children: [
+                SizedBox(
+                  height: 3.h,
+                ),
+                Align(
+                  alignment: Alignment.center,
+                  child: Container(
+                    height: 8,
+                    width: Dimensions.width * .2,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(16),
+                        color: Colors.grey[350]!.withOpacity(.9)),
+                  ),
+                ),
+                /**
+                * one -friendly tip
+                */
+                FriendlyTipsItem(
+                  tipImagePath: 'assets/images/friendly_tips_images/socket.png',
+                  tipTitle: "Switch off your plug sockets",
+                  tipSubtitle:
+                      "Save energy - and money! - by turning off your plugs.",
+                ),
+
+                /**
+                * two -friendly tip
+                */
+                FriendlyTipsItem(
+                  tipImagePath: 'assets/images/friendly_tips_images/oven.png',
+                  tipTitle: "Use your microwave",
+                  tipSubtitle:
+                      "microwaves are a lot more energy-efficient than conventional ovens.",
+                ),
+
+                /**
+                * three -friendly tip
+                */
+
+                FriendlyTipsItem(
+                  tipImagePath:
+                      'assets/images/friendly_tips_images/plastic.png',
+                  tipTitle: "Reduce your plastic consumption",
+                  tipSubtitle:
+                      "why not pick up loose fruit and vegetable, instead of pre-wrapped packs? And don't forget your canvas bag!.",
+                ),
+
+                /**
+                * four -friendly tip
+                */
+
+                FriendlyTipsItem(
+                  tipImagePath:
+                      'assets/images/friendly_tips_images/water-bottle.png',
+                  tipTitle:
+                      "Keep a reusable water bottle and coffee cup on you",
+                  tipSubtitle:
+                      "This will help you reduce your use of single-waste plastic.",
+                ),
+
+                /**
+                * five -friendly tip
+                */
+                FriendlyTipsItem(
+                  tipImagePath:
+                      'assets/images/friendly_tips_images/vehicles.png',
+                  tipTitle: "Use public transport",
+                  tipSubtitle:
+                      "A car may be the easiest and fastest option, but by taking the bus or train, or choosing to cycle or walk, you will help to pollute less.",
+                ),
+
+                /**
+                * six -friendly tip
+                */
+
+                FriendlyTipsItem(
+                  tipImagePath: 'assets/images/friendly_tips_images/socket.png',
+                  tipTitle: "Switch off your plug sockets",
+                  tipSubtitle:
+                      "Save energy - and money! - by turning off your plugs.",
+                ),
+                /**
+                * seven -friendly tip
+                */
+                FriendlyTipsItem(
+                  tipImagePath: 'assets/images/friendly_tips_images/oven.png',
+                  tipTitle: "Use your microwave",
+                  tipSubtitle:
+                      "microwaves are a lot more energy-efficient than conventional ovens.",
+                ),
+
+                /**
+                * eight -friendly tip
+                */
+                FriendlyTipsItem(
+                  tipImagePath:
+                      'assets/images/friendly_tips_images/plastic.png',
+                  tipTitle: "Reduce your plastic consumption",
+                  tipSubtitle:
+                      "why not pick up loose fruit and vegetable, instead of pre-wrapped packs? And don't forget your canvas bag!.",
+                ),
+                /**
+                * nine -friendly tip
+                */
+                FriendlyTipsItem(
+                  tipImagePath:
+                      'assets/images/friendly_tips_images/water-bottle.png',
+                  tipTitle:
+                      "Keep a reusable water bottle and coffee cup on you",
+                  tipSubtitle:
+                      "This will help you reduce your use of single-waste plastic.",
+                ),
+                /**
+                * ten -friendly tip
+                */
+                FriendlyTipsItem(
+                  tipImagePath:
+                      'assets/images/friendly_tips_images/vehicles.png',
+                  tipTitle: "Use public transport",
+                  tipSubtitle:
+                      "A car may be the easiest and fastest option, but by taking the bus or train, or choosing to cycle or walk, you will help to pollute less.",
+                ),
+                /**
+                * eleven -friendly tip
+                */
+
+                FriendlyTipsItem(
+                  tipImagePath: 'assets/images/friendly_tips_images/socket.png',
+                  tipTitle: "Switch off your plug sockets",
+                  tipSubtitle:
+                      "Save energy - and money! - by turning off your plugs.",
+                ),
+                /**
+                * twelve -friendly tip
+                */
+                FriendlyTipsItem(
+                  tipImagePath: 'assets/images/friendly_tips_images/oven.png',
+                  tipTitle: "Use your microwave",
+                  tipSubtitle:
+                      "microwaves are a lot more energy-efficient than conventional ovens.",
+                ),
+
+                /**
+                * thirteen -friendly tip
+                */
+                FriendlyTipsItem(
+                  tipImagePath:
+                      'assets/images/friendly_tips_images/plastic.png',
+                  tipTitle: "Reduce your plastic consumption",
+                  tipSubtitle:
+                      "why not pick up loose fruit and vegetable, instead of pre-wrapped packs? And don't forget your canvas bag!.",
+                ),
+                /**
+                * fourteenth -friendly tip
+                */
+                FriendlyTipsItem(
+                  tipImagePath:
+                      'assets/images/friendly_tips_images/water-bottle.png',
+                  tipTitle:
+                      "Keep a reusable water bottle and coffee cup on you",
+                  tipSubtitle:
+                      "This will help you reduce your use of single-waste plastic.",
+                ),
+                /**
+                * ten -friendly tip
+                */
+                FriendlyTipsItem(
+                  tipImagePath:
+                      'assets/images/friendly_tips_images/vehicles.png',
+                  tipTitle: "Use public transport",
+                  tipSubtitle:
+                      "A car may be the easiest and fastest option, but by taking the bus or train, or choosing to cycle or walk, you will help to pollute less.",
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
--- green_egypt/lib/screens/home_page/waste_category_view.dart
+++ green_egypt/lib/screens/home_page/waste_category_view.dart
@@ -14,239 +14,238 @@ class WasteCategoryView extends StatelessWidget {
    */
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Color(0xfff7f5f5),
-      body: Container(
+    List<Widget> list = [
+      Container(
         width: Dimensions.width,
-        height: Dimensions.height,
-        child: Column(
-          children: [
-            /**
-                 * first container
-               */
-            Container(
-              width: Dimensions.width,
-              height: Dimensions.height * 0.45,
-              color: Color(0xfff7f5f5),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
+        // height: Dimensions.height * 0.55,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(32), topRight: Radius.circular(32)),
+          color: Color(0xfffefefc),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.only(left: 16, right: 16),
+          child: ListView(
+            scrollDirection: Axis.vertical,
+            children: [
+              SizedBox(
+                height: Dimensions.height * .03,
+              ),
+              Align(
+                alignment: Alignment.center,
+                child: Container(
+                  height: 8,
+                  width: Dimensions.width * .2,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(16),
+                      color: Colors.grey[350]!.withOpacity(.9)),
+                ),
+              ),
+              SizedBox(
+                height: Dimensions.height * .03,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
                 children: [
                   SizedBox(
-                    height: Dimensions.height * .05,
+                    width: Dimensions.width * 0.04,
                   ),
-                  Padding(
-                    padding: EdgeInsets.only(left: Dimensions.width * .03),
-                    child: TextButton(
-                      onPressed: () {
-                        Get.back();
-                      },
-                      child: Container(
-                        padding: EdgeInsets.all(8),
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10),
-                          color: Colors.grey[350]!.withOpacity(.8),
-                        ),
-                        child: Icon(
-                          Icons.arrow_back_rounded,
-                          size: 22.sp,
-                          color: Colors.white,
-                        ),
-                      ),
+                  Text(
+                    "How to recycle ${heroTag}",
+                    style: TextStyle(
+                      fontWeight: FontWeight.w100,
+                      fontSize: 22.sp,
                     ),
                   ),
-                  Hero(
-                    tag: heroTag,
-                    child: Padding(
-                      padding: EdgeInsets.only(
-                          left: Dimensions.width * .23,
-                          top: Dimensions.height * .03),
-                      child: Image(
-                        width: 53.w,
-                        image: AssetImage(
-                          imagePath,
-                        ),
-                      ),
-                    ),
-                  )
                 ],
               ),
-            ),
 
-            /**
-                 ******************************* second container ****************************
-                  */
-
-            Container(
-              width: Dimensions.width,
-              height: Dimensions.height * 0.55,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(32),
-                    topRight: Radius.circular(32)),
-                color: Color(0xfffefefc),
-              ),
-              child: Padding(
-                padding: const EdgeInsets.only(left: 16, right: 16),
-                child: ListView(
-                  scrollDirection: Axis.vertical,
-                  children: [
-                    SizedBox(
-                      height: Dimensions.height * .01,
-                    ),
-                    Align(
-                      alignment: Alignment.center,
-                      child: Container(
-                        height: 8,
-                        width: Dimensions.width * .2,
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(16),
-                            color: Colors.grey[350]!.withOpacity(.9)),
-                      ),
-                    ),
-                    SizedBox(
-                      height: Dimensions.height * .03,
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        SizedBox(
-                          width: Dimensions.width * 0.04,
-                        ),
-                        Text(
-                          "How to recycle ${heroTag}",
-                          style: TextStyle(
-                            fontWeight: FontWeight.w100,
-                            fontSize: 22.sp,
-                          ),
-                        ),
-                      ],
-                    ),
-
-                    /**
+              /**
                    * first tip to how to recycle paper
                     */
 
-                    SizedBox(
-                      height: Dimensions.width * .04,
-                    ),
-                    ListTile(
-                      contentPadding: EdgeInsets.only(
-                        top: Dimensions.height * .001,
-                        left: Dimensions.width * .03,
-                        right: Dimensions.width * .02,
-                      ),
-                      leading: Text(
-                        "1.",
-                        style: TextStyle(
-                            color: Color(0xffa0cc00),
-                            fontWeight: FontWeight.bold,
-                            fontSize: 18.sp),
-                      ),
-                      title: Text(
-                        "Find out the difference between recyclable and non-recyclable paper. Such paper as waxed, laminated or painted is not recyclable.But you can recycle newspapers, magazines, packaging, cardboard, envelopes , office paper and any other paper of this type .",
-                        // textAlign: TextAlign.left,
-                        style: TextStyle(
-                          fontSize: 17.sp,
-                          color: Color.fromARGB(255, 122, 116, 116),
-                        ),
-                      ),
-                    ),
+              SizedBox(
+                height: Dimensions.width * .04,
+              ),
+              ListTile(
+                contentPadding: EdgeInsets.only(
+                  top: Dimensions.height * .001,
+                  left: Dimensions.width * .03,
+                  right: Dimensions.width * .02,
+                ),
+                leading: Text(
+                  "1.",
+                  style: TextStyle(
+                      color: Color(0xffa0cc00),
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18.sp),
+                ),
+                title: Text(
+                  "Find out the difference between recyclable and non-recyclable paper. Such paper as waxed, laminated or painted is not recyclable.But you can recycle newspapers, magazines, packaging, cardboard, envelopes , office paper and any other paper of this type .",
+                  // textAlign: TextAlign.left,
+                  style: TextStyle(
+                    fontSize: 17.sp,
+                    color: Color.fromARGB(255, 122, 116, 116),
+                  ),
+                ),
+              ),
 
-                    /**
+              /**
                    * Second tip to how to recycle paper
                     */
 
-                    SizedBox(
-                      height: Dimensions.width * .04,
-                    ),
-                    ListTile(
-                      contentPadding: EdgeInsets.only(
-                        top: Dimensions.height * .001,
-                        left: Dimensions.width * .03,
-                        right: Dimensions.width * .02,
-                      ),
-                      leading: Text(
-                        "2.",
-                        style: TextStyle(
-                            color: Color(0xffa0cc00),
-                            fontWeight: FontWeight.bold,
-                            fontSize: 18.sp),
-                      ),
-                      title: Text(
-                        "Remove all dirty or greasy paper. The paper is recycled using water. In the presence of pollutants , they can spoil the entire batch, regardless of whether they are soluble in water or not.",
-                        // textAlign: TextAlign.left,
-                        style: TextStyle(
-                          fontSize: 17.sp,
-                          color: Color.fromARGB(255, 122, 116, 116),
-                        ),
-                      ),
-                    ),
-
-                    /**
-                   * Third tip to how to recycle paper
-                    */
-
-                    SizedBox(
-                      height: Dimensions.width * .04,
-                    ),
-                    ListTile(
-                      contentPadding: EdgeInsets.only(
-                        top: Dimensions.height * .001,
-                        left: Dimensions.width * .03,
-                        right: Dimensions.width * .02,
-                      ),
-                      leading: Text(
-                        "3.",
-                        style: TextStyle(
-                            color: Color(0xffa0cc00),
-                            fontWeight: FontWeight.bold,
-                            fontSize: 18.sp),
-                      ),
-                      title: Text(
-                        "Sort the paper . Different types of paper have a different fiber length, and thus a different quality. That's why it's important to have them sorted by type. Cardboard goes to cardboard, office paper goes to office paper, etc.",
-                        // textAlign: TextAlign.left,
-                        style: TextStyle(
-                          fontSize: 17.sp,
-                          color: Color.fromARGB(255, 122, 116, 116),
-                        ),
-                      ),
-                    ),
-
-                    /**
-                   * Third tip to how to recycle paper
-                    */
-                    SizedBox(
-                      height: Dimensions.width * .04,
-                    ),
-                    ListTile(
-                      contentPadding: EdgeInsets.only(
-                        top: Dimensions.height * .001,
-                        left: Dimensions.width * .03,
-                        right: Dimensions.width * .02,
-                      ),
-                      leading: Text(
-                        "4.",
-                        style: TextStyle(
-                            color: Color(0xffa0cc00),
-                            fontWeight: FontWeight.bold,
-                            fontSize: 18.sp),
-                      ),
-                      title: Text(
-                        "Bring your sorted paper to the closest recycling centre.",
-                        // textAlign: TextAlign.left,
-                        style: TextStyle(
-                          fontSize: 17.sp,
-                          color: Color.fromARGB(255, 122, 116, 116),
-                        ),
-                      ),
-                    ),
-                  ],
+              SizedBox(
+                height: Dimensions.width * .04,
+              ),
+              ListTile(
+                contentPadding: EdgeInsets.only(
+                  top: Dimensions.height * .001,
+                  left: Dimensions.width * .03,
+                  right: Dimensions.width * .02,
+                ),
+                leading: Text(
+                  "2.",
+                  style: TextStyle(
+                      color: Color(0xffa0cc00),
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18.sp),
+                ),
+                title: Text(
+                  "Remove all dirty or greasy paper. The paper is recycled using water. In the presence of pollutants , they can spoil the entire batch, regardless of whether they are soluble in water or not.",
+                  // textAlign: TextAlign.left,
+                  style: TextStyle(
+                    fontSize: 17.sp,
+                    color: Color.fromARGB(255, 122, 116, 116),
+                  ),
                 ),
               ),
-            )
-          ],
+
+              /**
+                   * Third tip to how to recycle paper
+                    */
+
+              SizedBox(
+                height: Dimensions.width * .04,
+              ),
+              ListTile(
+                contentPadding: EdgeInsets.only(
+                  top: Dimensions.height * .001,
+                  left: Dimensions.width * .03,
+                  right: Dimensions.width * .02,
+                ),
+                leading: Text(
+                  "3.",
+                  style: TextStyle(
+                      color: Color(0xffa0cc00),
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18.sp),
+                ),
+                title: Text(
+                  "Sort the paper . Different types of paper have a different fiber length, and thus a different quality. That's why it's important to have them sorted by type. Cardboard goes to cardboard, office paper goes to office paper, etc.",
+                  // textAlign: TextAlign.left,
+                  style: TextStyle(
+                    fontSize: 17.sp,
+                    color: Color.fromARGB(255, 122, 116, 116),
+                  ),
+                ),
+              ),
+
+              /**
+                   * Third tip to how to recycle paper
+                    */
+              SizedBox(
+                height: Dimensions.width * .04,
+              ),
+              ListTile(
+                contentPadding: EdgeInsets.only(
+                  top: Dimensions.height * .001,
+                  left: Dimensions.width * .03,
+                  right: Dimensions.width * .02,
+                ),
+                leading: Text(
+                  "4.",
+                  style: TextStyle(
+                      color: Color(0xffa0cc00),
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18.sp),
+                ),
+                title: Text(
+                  "Bring your sorted paper to the closest recycling centre.",
+                  // textAlign: TextAlign.left,
+                  style: TextStyle(
+                    fontSize: 17.sp,
+                    color: Color.fromARGB(255, 122, 116, 116),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
-      ),
-    );
+      )
+    ];
+    return Scaffold(
+        backgroundColor: Color(0xfff7f5f5),
+        body: SafeArea(
+          child: NestedScrollView(
+              headerSliverBuilder: (context, innerBoxIsScrolled) {
+                return [
+                  SliverAppBar(
+                    automaticallyImplyLeading: false,
+                    centerTitle: true,
+                    expandedHeight: Dimensions.height * 0.45,
+                    backgroundColor: Color(0xfff7f5f5),
+                    flexibleSpace: Container(
+                      child: FlexibleSpaceBar(
+                        background: Stack(
+                          children: [
+                            Positioned(
+                              top: 10,
+                              left: 20,
+                              child: TextButton(
+                                onPressed: () {
+                                  Get.back();
+                                },
+                                child: Container(
+                                  padding: EdgeInsets.all(8),
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(10),
+                                    color: Colors.grey[350]!.withOpacity(.8),
+                                    // Colors.grey[350]!.withOpacity(.8)
+                                  ),
+                                  child: Icon(
+                                    Icons.arrow_back_rounded,
+                                    size: 22.sp,
+                                    color: Colors.black,
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Align(
+                              alignment: Alignment.center,
+                              child: Container(
+                                child: Hero(
+                                  tag: heroTag,
+                                  child: Image(
+                                    width: 72.w,
+                                    image: AssetImage(
+                                      imagePath,
+                                    ),
+                                    fit: BoxFit.cover,
+                                    alignment: Alignment.center,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+
+                        // background:
+                      ),
+                    ),
+                  )
+                ];
+              },
+              body: list[0]),
+        ));
   }
 }
