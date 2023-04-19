9d05a4f3a6b1b802f4fdf65768d1cf7ce8969be6
Mohamad Amgad <mohamadamgad09@gmail.com>
2023-04-19 14:06:40 +0200

    app lang storing and changing fully implemented

--- green_egypt/lib/config/localization/locale_controller.dart
+++ green_egypt/lib/config/localization/locale_controller.dart
@@ -3,11 +3,8 @@ import 'dart:ui';
 import 'package:get/get.dart';
 
 class localController extends GetxController {
- // Locale initallang= sharepref!.getString("lang")=="ar"?Locale("ar"):Locale("en");
   void changeLang(String codelang) {
     Locale locale = Locale(codelang);
- // sharepref!.setString("lang",codelang);
-    
     Get.updateLocale(locale);
   }
 }
--- green_egypt/lib/services/Shared_preferences/shared_preferences_class.dart
+++ green_egypt/lib/services/Shared_preferences/shared_preferences_class.dart
@@ -1,5 +1,4 @@
-import 'package:get/get.dart';
-import 'package:green_egypt/config/theme/application_theme_controller_box.dart';
+import 'package:green_egypt/config/localization/language_box.dart';
 import 'package:green_egypt/services/Shared_preferences/first_launch.dart';
 import 'package:green_egypt/services/boxes/user_data_db.dart';
 
@@ -9,6 +8,11 @@ class SharedPreferencesClass {
     // await ApplicationThemeController.instance
     //     .initApplicationThemeController_ThemeAndBox();
 
+
+    /**
+     * init language box to retrieve last stored language on application .
+     */
+    await LanguageBox.instance.initLanguageBox();
     // user Data Box that contain all user data stored in .
     // boxes are local storing objects as Shared Preferences put with extra speed .
     UserDataBox.instance.initiateUserBox();
